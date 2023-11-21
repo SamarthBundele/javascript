f8b7d9e8bd6ad26a0a864b21c1872cce5024a849
SamarthBundele <100133427+SamarthBundele@users.noreply.github.com>
2023-11-21 20:35:09 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId=1
+let accountEmail='samarth@gmail.com'
+accountCity='Mumbai'
+let accountStatus
+
+// accountId=13 not allowed 
+accountEmail='samarth1234@gmail.com'
+accountCity='Pune'
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountStatus]);
