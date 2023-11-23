151b579b0c03b7f1a9240070bb53a32de33884a3
SamarthBundele <100133427+SamarthBundele@users.noreply.github.com>
2023-11-23 19:34:09 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+let num1=32
+let name1='samarth'
+let login=true
+let temp=null
+let call
+
+console.log(typeof num1);
+console.log(typeof name1);
+console.log(typeof login);
+console.log(typeof temp);
+console.log(typeof call);
+
+let num2='322'
+let num3=Number(num2)
+console.log(num3);
