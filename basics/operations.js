9750ef64a20c5b31a3466c6f93c5b62c4f22b188
SamarthBundele <100133427+SamarthBundele@users.noreply.github.com>
2023-11-23 20:27:32 +0000

    operations

---
+++ basics/operations.js
@@ -0,0 +1,13 @@
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2**3);
+console.log(2/3);
+
+let num1=3
+let num2=-num1
+console.log(num2);
+
+let count = 100
+count++
+console.log(count);
