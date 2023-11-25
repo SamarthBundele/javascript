ccb2dfacd1f61cef3f9c49655d1d6a6ca7046d2f
SamarthBundele <100133427+SamarthBundele@users.noreply.github.com>
2023-11-25 18:14:36 +0000

    memory management

---
+++ basics/memory.js
@@ -0,0 +1,15 @@
+//stack memory and heap memory
+
+let name1 = 'samarth'
+let name2=name1
+name2='bundele'
+console.log(name1,name2);
+
+let user1 = {
+    name:'samarth',
+    email:'samarthbundele@gmail.com',
+    pass:'hello123'
+}
+let user2=user1
+user2.name='samarth bundele'
+console.log(user1,user2);
