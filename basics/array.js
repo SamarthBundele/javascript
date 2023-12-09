8f9c9a908abb9d314abfffe535a54146718055c7
SamarthBundele <100133427+SamarthBundele@users.noreply.github.com>
2023-12-09 19:16:56 +0000

    array

---
+++ basics/array.js
@@ -0,0 +1,23 @@
+const myArr=[0,1,2,3,4]
+const myArr2=["samarth","bundele"]
+
+const myarr3=new Array(2,3,4,5)
+
+console.log(myArr[2]);
+
+//array methods
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+
+
+console.log(myArr.slice(1,3));
+console.log(myArr);
+
+console.log(myArr.splice(1,3));
+console.log(myArr);
